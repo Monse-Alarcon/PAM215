474ba2d493067c8b73cb4d9903c5ebf0e0eeb309
Monse-Alarcón <124050107@upq.edu.mx>
2025-09-05 20:26:30 -0600

    Todo esto es de neatbeans ya con el código bien

---
+++ Codigo1/inicioPAM/src/iniciopam/InicioPAM.java
@@ -0,0 +1,9 @@
+package iniciopam;
+
+public class InicioPAM {
+    // Método para mostrar el reglamento POO
+    public void mostrarReglamentoPOO() {
+        System.out.println("=== REGLAMENTO POO ===");
+    }
+    
+}
