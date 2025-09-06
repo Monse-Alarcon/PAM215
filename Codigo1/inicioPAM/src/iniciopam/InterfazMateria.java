5d1696cba48cbc10cca301ce703ee047ef391961
Monse-Alarcón <124050107@upq.edu.mx>
2025-09-05 21:31:15 -0600

    Creación de la ventana

--- Codigo1/inicioPAM/src/iniciopam/InterfazMateria.java
+++ Codigo1/inicioPAM/src/iniciopam/InterfazMateria.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package iniciopam;
 
-/**
- *
- * @author Monsee
- */
-public class InterfazMateria {
+package iniciopam;
+import javax.swing.*;
+
+public class InterfazMateria extends JFrame {
     
+    public InterfazMateria() {
+    }
 }
