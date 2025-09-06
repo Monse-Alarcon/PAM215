1b14ed063409a97b5d453ef915ebfb5fa24c0d75
Monse-Alarcón <124050107@upq.edu.mx>
2025-09-05 21:36:32 -0600

    Configuración de la ventana

--- Codigo1/inicioPAM/src/iniciopam/InterfazMateria.java
+++ Codigo1/inicioPAM/src/iniciopam/InterfazMateria.java
@@ -4,6 +4,19 @@ import javax.swing.*;
 
 public class InterfazMateria extends JFrame {
     
+    private InicioPAM materia = new InicioPAM();
+    
     public InterfazMateria() {
+        // 1. Poner título a la ventana
+        setTitle("Información de POO");
+    
+        // 2. Definir el tamaño (ancho, alto)
+        setSize(500, 400);
+
+        // 3. Qué pasa al cerrar la ventana
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // 4. Centrar la ventana en la pantalla  
+        setLocationRelativeTo(null);
     }
 }
