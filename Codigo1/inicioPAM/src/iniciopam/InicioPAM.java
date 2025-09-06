80ae34e281cddd49fcad280b20e23a6ea53e292e
Monse-Alarcón <124050107@upq.edu.mx>
2025-09-05 21:59:05 -0600

    Darle forma al texto
    Quité algunos acentos o cambié palabras y agregué espacios para que se vea mejor

--- Codigo1/inicioPAM/src/iniciopam/InicioPAM.java
+++ Codigo1/inicioPAM/src/iniciopam/InicioPAM.java
@@ -6,14 +6,15 @@ public class InicioPAM {
         System.out.println("=== REGLAMENTO POO ===");
         System.out.println("1.Se requiere 80% de asistencia para evaluar");
         System.out.println("2.Se permiten 10 minutos de tolerancia");
-        System.out.println("3. Las faltas deberán estar justificadas");
-        System.out.println("4. Las tareas y trabajos deberán subirlas al Classroom");
+        System.out.println("3. Las faltas deben estar justificadas");
+        System.out.println("4. Las tareas y trabajos subirlas al Classroom");
         System.out.println("5. Las tareas y trabajos presentarlos en tiempo y forma");
         System.out.println("6. Utilizar el correo institucional");
         System.out.println("7. NO plagio o copia de trabajos");
-        System.out.println("8. Cualquier deshonestidad académica será reprobación");
+        System.out.println("8. Cualquier deshonestidad va a reprobar el alumno");
         System.out.println("9. Disciplina en todo momento");
         System.out.println("10. Uso de laptops solo cuando sea necesario");
+        System.out.println();
     }
     
     // Método para mostrar lineamientos de Classroom
@@ -23,6 +24,7 @@ public class InicioPAM {
         System.out.println("2. Entregar tareas en formato PDF");
         System.out.println("3. Usar nombres claros en archivos");
         System.out.println("4. No copiar");
+        System.out.println();
     }
     
       // Método para mostrar fechas de parciales
@@ -32,6 +34,7 @@ public class InicioPAM {
         System.out.println("2do Parcial: 03-11-25");
         System.out.println("3er Parcial: 01-12-25");
         System.out.println("Final: 08-12-25");
+        System.out.println();
     }
     
      // Método para mostrar porcentajes por parcial
@@ -39,21 +42,24 @@ public class InicioPAM {
         System.out.println("=== PORCENTAJES POR PARCIAL ===");
         System.out.println("1er Parcial:");
         System.out.println("Conocimento: 40%");
-        System.out.println("Desempeño: 20%");
+        System.out.println("Desempeno: 20%");
         System.out.println("Producto: 30%");
         System.out.println("PI: 10%");
+        System.out.println();
         
         System.out.println("2do Parcial:");
         System.out.println("Conocimento: 40%");
-        System.out.println("Desempeño: 20%");
+        System.out.println("Desempeno: 20%");
         System.out.println("Producto: 20%");
         System.out.println("PI: 20%");
+        System.out.println();
         
         System.out.println("3er Parcial:");
         System.out.println("Conocimento: 20%");
-        System.out.println("Desempeño: 10%");
+        System.out.println("Desempeno: 10%");
         System.out.println("Producto: 40%");
         System.out.println("PI: 30%");
+        System.out.println();
     }
     
 }
