e1ce66f70b408bed7d329ae7b01a1ac1c91cf6b6
Monse-Alarcón <124050107@upq.edu.mx>
2025-09-22 20:22:23 -0600

    Ejercicio A

---
+++ Practica2/EjA.js
@@ -0,0 +1,11 @@
+const persona = {
+    nombre: "Monserrath",
+    edad: 19,
+    direccion: {
+        ciudad: "Qro",
+        pais: "MX"
+    }
+};
+
+const { nombre, edad, direccion: { ciudad } } = persona;
+console.log("Me llamo " + nombre + ", tengo " + edad + " años y vivo en " + ciudad+ ".");
