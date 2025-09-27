ddf82c5904f3b59f0c0c985f7a4d8c0b00bd114f
Monse-Alarcón <124050107@upq.edu.mx>
2025-09-26 19:54:24 -0600

    Ejercicio A

---
+++ Practica3/main.js
@@ -0,0 +1,5 @@
+import{restar} from "./utils.js";
+
+console.log("La resta de 10 - 5 = ", restar(10,5));
+console.log("La resta de 80 - 7 = ", restar(80,7));
+console.log("La resta de 700 - 90 = ", restar(700,90));
