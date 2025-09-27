413e5272e3d07869577ad2375678ffca419df4e6
Monse-Alarcón <124050107@upq.edu.mx>
2025-09-26 20:05:33 -0600

    Ejercicio B

---
+++ Practica3/EjB.js
@@ -0,0 +1,18 @@
+function verfificarUsuario(usuario) {
+    return new Promise((resolve, reject) => {
+    if (usuario === "admin") {
+        resolve("Acceso concedido");
+    } else {
+        reject("Acceso denegado");
+    }
+    });
+    }
+
+    verfificarUsuario("admin")
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+
+    verfificarUsuario("Ivan")
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+    
